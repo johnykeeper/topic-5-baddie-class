684bf7a9688337d53a7a7e9758a27a444b845451
362568651 <362568651@2260I-L29-003>
2026-04-13 13:03:19 -0400

    almost done!!!!!

--- Game1.cs
+++ Game1.cs
@@ -19,8 +19,11 @@ namespace topic_5_baddie_class
         Texture2D titleTexture;
         Texture2D endTexture;
         Texture2D marioTexture;
+        List<Ghost> ghosts;
 
         MouseState mouseState;
+
+        KeyboardState keyboardState;
 
         Random random;
 
@@ -37,7 +40,7 @@ namespace topic_5_baddie_class
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
@@ -47,8 +50,12 @@ namespace topic_5_baddie_class
             window = new Rectangle(0, 0, 800, 600);
             random = new Random();
             ghostTextures = new List<Texture2D>();
-
-
+            ghosts = new List<Ghost>();
+            for(int i = 0; i < 20; i++)
+            {
+                ghosts.Add(new Ghost(ghostTextures, new Rectangle(random.Next(0, 760), random.Next(0, 560), 40, 40)));
+            }
+            screen = Screen.Title;
 
             base.Initialize();
         }
@@ -80,6 +87,21 @@ namespace topic_5_baddie_class
             // TODO: Add your update logic here
 
             mouseState = Mouse.GetState();
+            keyboardState = Keyboard.GetState();
+            if(screen == Screen.Title)
+            {
+                if (keyboardState.IsKeyDown(Keys.Enter))
+                    screen = Screen.House;
+            }
+            else if(screen == Screen.House)
+            {
+                foreach(Ghost g in ghosts)
+                {
+                    g.Update(gameTime, mouseState);
+                    if (g.Intersects(new Rectangle(mouseState.X, mouseState.Y, 40, 40)))
+                       screen = Screen.End;
+                }
+            }
 
 
 
@@ -90,12 +112,19 @@ namespace topic_5_baddie_class
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            // TODO: Add your drawing code here
+            if (screen == Screen.Title)
+                _spriteBatch.Draw(titleTexture, window, Color.White);
+            else if(screen == Screen.House)
+            {
+                _spriteBatch.Draw(backgroundTexture, window, Color.White);
+                foreach (Ghost g in ghosts)
+                    g.Draw(_spriteBatch);
+                _spriteBatch.Draw(marioTexture, new Rectangle(mouseState.X, mouseState.Y, 40, 40), Color.White);
+            }
+            else
+                _spriteBatch.Draw(endTexture, window, Color.White);
 
-
-            _spriteBatch.Draw(backgroundTexture, window, Color.White);
-
-            _spriteBatch.End();
+                _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ Ghost.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topic_5_baddie_class
+{
+    public class Ghost
+    {
+        private List<Texture2D> _textures;
+        private Vector2 _speed;
+        private Rectangle _location;
+        private int _textureindex;
+        private SpriteEffects _direction;
+        private float _animatedSpeed;
+        private float _seconds;
+
+        public Rectangle Rect
+        {
+            get { return _location; }
+        }
+        public Ghost(List <Texture2D> textures, Rectangle location)
+        {
+            _textures = textures;
+            _textureindex = 0;
+            _speed = Vector2.Zero;
+            _location = location;
+            _direction = SpriteEffects.None;
+            _animatedSpeed = 0.2f;
+            _seconds = 0;
+
+        }
+
+        public void Update(GameTime gameTime, MouseState mouseState)
+        {
+            _speed = Vector2.Zero;
+
+            if(mouseState.X < _location.X)
+            {
+                _direction = SpriteEffects.FlipHorizontally;
+                _speed.X = -1;
+            }
+            else if (mouseState.X > _location.X)
+            {
+                _direction = SpriteEffects.None;
+                _speed.X = 1;
+            }
+            if(mouseState.Y < _location.Y)
+            {
+                _speed.Y = -1;
+            }
+            if(mouseState.Y > _location.Y)
+            {
+                _speed.Y = 1;
+            }
+            if (mouseState.LeftButton == ButtonState.Released)
+            {
+                _speed = Vector2.Zero;
+                _textureindex = 0;
+                _seconds = 0f;
+            }
+            else if(_speed != Vector2.Zero)
+            {
+                _seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if(_seconds > _animatedSpeed)
+                {
+                    _seconds = 0;
+                    _textureindex++;
+                    if(_textureindex >= _textures.Count)
+                        _textureindex = 1;
+                }
+            }
+                _location.Offset(_speed);
+
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_textures[_textureindex], _location, null, Color.White, 0f, Vector2.Zero, _direction, 1);
+
+        }
+        public bool Contains(Point player)
+        {
+            return _location.Contains(player);
+        }
+        public bool Intersects(Rectangle player)
+        {
+            return _location.Intersects(player);
+        }
+    }
+}
